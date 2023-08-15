76603b4dcf9e92ac708900e58f2d35b3cbf84dba
Abuzar Yousaf <abuzaryousaf.123456789@gmail.com>
2023-08-15 20:13:58 +0500

    resumemadehalf

--- pages/Jobs.jsx
+++ pages/Jobs.jsx
@@ -2,6 +2,7 @@ import {Image, TextInput, Text, Pressable, FlatList, SafeAreaView, ScrollView} f
 import React from 'react'
 import {View} from 'react-native'
 import {useNavigation} from "@react-navigation/native";
+import Resume from "./Resume";
 
 const data = [
     {"name": "Facebook"},
@@ -23,7 +24,7 @@ function Jobs({navigation}) {
                         tintColor: '#000'
                     }} source={require('../assets/back_arrow.png')} alt={'Okay'}/></Pressable>
                     <View style={{width: '100%', marginTop: 0, paddingEnd: 90}}>
-                        <Pressable onPress={() => navigation.push('Cities')}><Image
+                        <Pressable onPress={() => navigation.push('Resume')}><Image
                             style={{width: 150, height: 40, marginTop: 60, alignSelf: 'center'}}
                             source={require('../assets/logo.png')} alt={'Okay'}/></Pressable>
                     </View>
