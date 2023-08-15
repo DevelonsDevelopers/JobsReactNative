02a8856889afdfb655bb5105b75ac78e41992687
Abuzar Yousaf <abuzaryousaf.123456789@gmail.com>
2023-08-15 22:47:00 +0500

    resumemade

--- App.js
+++ App.js
@@ -83,7 +83,7 @@ function App() {
                     <Stack.Screen name="History" component={History} options={{title: "History", headerShown: false}}/>
                     <Stack.Screen name="Jobs" component={Jobs} options={{title: "Jobs", headerShown: false}}/>
                     <Stack.Screen name="Resume" component={Resume} options={{title: "Resume", headerShown: false}}/>
-                    <Stack.Screen name="offers" component={Offers} options={{title: "Offers", headerShown: false}}/>
+                    <Stack.Screen name="Offers" component={Offers} options={{title: "Offers", headerShown: false}}/>
                 </Stack.Navigator>
             </NavigationContainer>
         </Provider>
