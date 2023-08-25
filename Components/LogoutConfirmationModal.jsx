ce82e948d0699f2ef704ecd3a0ec258644dd9d2e
Tayyab Mazhar <tayyabrana213@gmail.com>
2023-08-26 01:27:43 +0500

    Modal Component

---
+++ Components/LogoutConfirmationModal.jsx
@@ -0,0 +1,23 @@
+import {Image, Modal, Text, View} from "react-native";
+import React from "react";
+
+const LogoutConfirmationModal = ({visible, Logout, toggleLoadingVisibility}) => {
+  return (
+      <Modal visible={visible} animationType={"fade"} transparent={true}>
+          <View style={{ flex: 1, alignContent: 'center', justifyContent: 'center', backgroundColor: 'rgba(66, 66, 66, 0.4)' }}>
+              <View style={{ margin: 35, elevation: 24, borderRadius: 15, backgroundColor: '#fff', opacity: 1, padding: 20, justifyContent: 'center', alignItems: 'center', marginHorizontal: 30 }}>
+                  <Image style={{ width: 50, height: 50 }} source={require('../assets/runn.png')} />
+                  <Text style={{ paddingVertical: 7, fontSize: 16, fontFamily: 'poppins_medium', marginTop: 10 }}>Are You Sure You Want To Quit?</Text>
+                  <View style={{ flexDirection: 'row', paddingBottom: 20, paddingTop: 10 }}>
+                      <Text onPress={async () => Logout()} style={{ backgroundColor: '#13A3E1', color: 'white', paddingHorizontal: 6, paddingVertical: 7, fontSize: 16, fontFamily: 'poppins_medium', width: 100, textAlign: 'center', borderRadius: 20 }}>Yes</Text>
+                      <Text onPress={() => toggleLoadingVisibility()} style={{ backgroundColor: 'white', color: 'black', paddingHorizontal: 6, paddingVertical: 7, fontSize: 16, fontFamily: 'poppins_medium', marginLeft: 40, width: 100, textAlign: 'center', borderRadius: 20, borderWidth: 1 }}>No</Text>
+
+                  </View>
+              </View>
+          </View>
+      </Modal>
+  )
+
+}
+
+export default LogoutConfirmationModal
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -5,6 +5,7 @@ import { AllCategories, FeaturedCategories } from "../API/actions/categoryAction
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {RecentJobs} from "../API/actions/jobActions";
 import NavigationDrawer from "../Components/NavigationDrawer";
+import LogoutConfirmationModal from "../Components/LogoutConfirmationModal";
 
 function Home({ navigation }) {
 
@@ -65,20 +66,7 @@ function Home({ navigation }) {
     return (
         <View style={{ flex: 1 }}>
             <NavigationDrawer visible={visible} navigation={navigation} toggleVisibility={toggleVisibility} isLogin={isLogin} toggleLoadingVisibility={toggleLoadingVisibility} />
-            <Modal visible={loadingVisible} animationType={"fade"} transparent={true}>
-                <View style={{ flex: 1, alignContent: 'center', justifyContent: 'center', backgroundColor: 'rgba(66, 66, 66, 0.4)' }}>
-                    <View style={{ margin: 35, elevation: 24, borderRadius: 15, backgroundColor: '#fff', opacity: 1, padding: 20, justifyContent: 'center', alignItems: 'center', marginHorizontal: 30 }}>
-                        <Image style={{ width: 50, height: 50 }} source={require('../assets/runn.png')} />
-                        <Text style={{ paddingVertical: 7, fontSize: 16, fontFamily: 'poppins_medium', marginTop: 10 }}>Are You Sure You Want To Quit?</Text>
-                        <View style={{ flexDirection: 'row', paddingBottom: 20, paddingTop: 10 }}>
-                            <Text onPress={async () => Logout()} style={{ backgroundColor: '#13A3E1', color: 'white', paddingHorizontal: 6, paddingVertical: 7, fontSize: 16, fontFamily: 'poppins_medium', width: 100, textAlign: 'center', borderRadius: 20 }}>Yes</Text>
-                            <Text onPress={() => toggleLoadingVisibility()} style={{ backgroundColor: 'white', color: 'black', paddingHorizontal: 6, paddingVertical: 7, fontSize: 16, fontFamily: 'poppins_medium', marginLeft: 40, width: 100, textAlign: 'center', borderRadius: 20, borderWidth: 1 }}>No</Text>
-
-                        </View>
-                    </View>
-                </View>
-            </Modal>
-
+            <LogoutConfirmationModal toggleLoadingVisibility={toggleLoadingVisibility} visible={loadingVisible} Logout={Logout} />
             <ScrollView style={{ flex: 1, backgroundColor: '#F1F1F1', marginBottom: -75 }}>
                 <View style={{ flexDirection: 'column', width: '100%', height: 240, backgroundColor: '#13A3E1' }}>
                     <View style={{ flexDirection: 'row', height: 130 }}>
