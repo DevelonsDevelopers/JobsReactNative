0271f487bfaba13e6d868844c72753b37f5b4991
username880 <abubakar.4983763@gmail.com>
2023-09-28 16:40:00 -0700

    merging

---
+++ Components/DeleteModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const DeleteModal = () => {
+  return (
+ <View>
+    <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus dolore asperiores consequuntur praesentium quasi autem dolorem ipsa impedit illum in provident, itaque quisquam at distinctio repellat, veritatis tenetur laboriosam cumque.</Text>
+ </View>
+  )
+}
+
+export default DeleteModal
