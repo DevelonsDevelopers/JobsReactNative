4a83e4ddcbe9d2219d9a87c6250cbfff17c73829
AhmadHassan <AhmadHassan25202@gmail.com>
2023-09-28 00:11:38 -0700

    Loading nodata success and error in actions

--- API/actions/coverLetterActions.js
+++ API/actions/coverLetterActions.js
@@ -1,4 +1,7 @@
 import {
+    COVER_LETTER_ERROR,
+    COVER_LETTER_LOADING,
+    COVER_LETTER_SUCCESS,
     GET_COVER_BY_USER,
     LOADING,
     SUCCESS
@@ -8,12 +11,15 @@ import * as api from "../index";
 export const CoverLetterByUser = (user, job) => async (dispatch) => {
     try {
         dispatch ({ type: LOADING })
+        dispatch ({ type: COVER_LETTER_LOADING })
         const { data: { data } } = await api.fetchCoverByUser(user, job);
         console.log(data)
         dispatch ({ type: GET_COVER_BY_USER, payload: { coverLetters: data } })
         dispatch ({ type: SUCCESS })
+        dispatch ({ type: COVER_LETTER_SUCCESS })
     } catch (error) {
         console.log(error)
+        dispatch(COVER_LETTER_ERROR)
     }
 }
 
